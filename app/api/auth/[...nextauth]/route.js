8f370ae2645c7b87a417360b3231a1026cce8057
Rae Gaines <rae@throneless.tech>
2025-03-07 11:48:53 -0500

    wip middleware and fix versioning for prisma auth adapter

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,3 @@
-import { NextResponse, NextRequest } from 'next/server';
-import { handlers, auth } from "@/auth";
+import { handlers } from "@/auth";
 
-export const { POST } = handlers;
-
-export const GET = auth((req) => {
-
-  console.log('============================');
-  console.log(req.auth);
-
-  console.log('============================');
-
-  if (req.auth) {
-    return NextResponse.next();
-  }
-
-  // Redirect to login page if not authenticated
-  return NextResponse.redirect(new URL('/error?error=NotAuthorized', req.url))
-})
+export const { GET, POST } = handlers;
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
-import NextAuth from "next-auth"
-import authConfig from "./auth.config"
-
-export const { auth: middleware } = NextAuth(authConfig)
+export { auth as middleware } from "@/auth"
 
 // Routes Middleware should not run on
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// }
