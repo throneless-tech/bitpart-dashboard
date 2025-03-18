78c92ae97f0b6dc9f76f2e1545eea1eb6f6b1545
rudietuesdays <rae@throneless.tech>
2025-03-17 20:09:53 -0400

    wip web socket connection

--- app/components/create.jsx
+++ app/components/create.jsx
@@ -181,9 +181,23 @@ export default function CreateBotFlow({ userId }) {
 
   }
 
+  // web sockets
   useEffect(() => {
-
-  }, [])
+    const ws = new WebSocket('ws://localhost:3000/api/socket');
+    ws.onopen = () => {
+      console.log('Connected to WebSocket');
+      ws.send('Hello, WebSocket!');
+    };
+    ws.onmessage = (event) => {
+      console.log('Message received:', event.data);
+    };
+    ws.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+    return () => {
+      ws.close();
+    };
+  }, []);
 
   useEffect(() => { }, [captchaContainer, createdBot, stepCount, watchAll]);
 
