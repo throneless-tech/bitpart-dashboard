012259b5d4738ffd05cb1c7f518980d12aa79a63
Rae Gaines <rae@throneless.tech>
2025-08-04 15:36:08 -0400

    #119 add authentication to server actions

--- app/actions/getUser.js
+++ app/actions/getUser.js
@@ -1,8 +1,16 @@
 "use server";
 
+import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
 
 export const getUser = async (username) => {
+  const session = await auth();
+  const sessionUsername = session?.user?.name;
+
+  if (sessionUsername !== username) {
+    return null;
+  }
+
   const user = await prisma.user.findUnique({
     where: {
       username,
