58e85f9e962ae629d689ee3b2c6acd74d4ef13b8
Rae Gaines <rae@throneless.tech>
2025-09-05 17:40:01 -0400

    fix: remove seo indexing from robots

--- app/robots.js
+++ app/robots.js
@@ -2,8 +2,8 @@ export default function robots() {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
-      disallow: ["/create/", "/my-bots/"],
+      // allow: "/",
+      disallow: ["/", "/create/", "/my-bots/"],
     },
     sitemap: "https://bitp.art/sitemap.xml",
   };
